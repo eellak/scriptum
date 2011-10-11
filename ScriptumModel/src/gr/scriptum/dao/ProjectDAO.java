432c528143924631440a8a9407fd236698d105b7
Angelos Anagnostopoulos <angelos@uit.gr>
2011-10-11 09:50:38 +0000

    Added DAO.

---
+++ ScriptumModel/src/gr/scriptum/dao/ProjectDAO.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package gr.scriptum.dao;
+
+import gr.scriptum.domain.Project;
+
+/**
+ * @author aanagnostopoulos
+ *
+ */
+public class ProjectDAO extends GenericDAO<Project, Integer> {
+
+}
