f149c8dce701efddb7791a878b37b506e7b494f5
Angelos Anagnostopoulos <angelos@uit.gr>
2011-10-11 09:48:49 +0000

    Implemented hashCode() and equals().

--- ScriptumModel/src/gr/scriptum/domain/UserHierarchy.java
+++ ScriptumModel/src/gr/scriptum/domain/UserHierarchy.java
@@ -72,4 +72,29 @@ public class UserHierarchy implements java.io.Serializable {
 		this.userHierarchies = userHierarchies;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserHierarchy other = (UserHierarchy) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
