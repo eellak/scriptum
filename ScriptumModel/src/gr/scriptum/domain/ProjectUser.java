521093c302852476ba322a15934a9c53183d7c90
Angelos Anagnostopoulos <angelos@uit.gr>
2011-10-12 10:42:24 +0000

    Implemented hashCode() and equals().

--- ScriptumModel/src/gr/scriptum/domain/ProjectUser.java
+++ ScriptumModel/src/gr/scriptum/domain/ProjectUser.java
@@ -71,4 +71,29 @@ public class ProjectUser implements java.io.Serializable {
 		this.updateTs = updateTs;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProjectUser other = (ProjectUser) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
