0749328b1c0703712d644b9314b5910cbafa03f0
Angelos Anagnostopoulos <angelos@uit.gr>
2011-10-16 09:39:28 +0000

    Added DAO.

---
+++ ScriptumModel/src/gr/scriptum/dao/TaskResultDAO.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package gr.scriptum.dao;
+
+import gr.scriptum.domain.TaskResult;
+
+/**
+ * @author aanagnostopoulos
+ *
+ */
+public class TaskResultDAO extends GenericDAO<TaskResult, Integer> {
+
+}
