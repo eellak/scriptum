6aaf963cf0a4861ef5fe967caa90b51be1d4c22b
Angelos Anagnostopoulos <angelos@uit.gr>
2011-10-17 13:03:40 +0000

    Added DAO.

---
+++ ScriptumModel/src/gr/scriptum/dao/ProjectTaskDAO.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package gr.scriptum.dao;
+
+import gr.scriptum.domain.ProjectTask;
+
+/**
+ * @author aanagnostopoulos
+ *
+ */
+public class ProjectTaskDAO extends GenericDAO<ProjectTask, Integer> {
+
+}
---
+++ ScriptumModel/src/gr/scriptum/dao/TaskDocumentDAO.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package gr.scriptum.dao;
+
+import gr.scriptum.domain.TaskDocument;
+
+/**
+ * @author aanagnostopoulos
+ *
+ */
+public class TaskDocumentDAO extends GenericDAO<TaskDocument, Integer> {
+
+}
