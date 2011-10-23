782401ab3b2b04300761592f1125d8c66654f6a2
Angelos Anagnostopoulos <angelos@uit.gr>
2011-10-23 15:46:01 +0000

    Implemented search methods.

--- ScriptumModel/src/gr/scriptum/domain/TaskMessage.java
+++ ScriptumModel/src/gr/scriptum/domain/TaskMessage.java
@@ -94,4 +94,29 @@ public class TaskMessage implements java.io.Serializable {
 		this.createdTs = createdTs;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TaskMessage other = (TaskMessage) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
