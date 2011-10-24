97bda7c3e0ba8bd1bf3b4cd7c389420828fcf7e8
Angelos Anagnostopoulos <angelos@uit.gr>
2011-10-24 08:00:04 +0000

    Added find by term methods.

--- ScriptumModel/src/gr/scriptum/dao/UsersDAO.java
+++ ScriptumModel/src/gr/scriptum/dao/UsersDAO.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.hibernate.Query;
 
+import gr.scriptum.domain.Contact;
 import gr.scriptum.domain.Users;
 
 /**
@@ -22,6 +23,35 @@ public class UsersDAO extends GenericDAO<Users, Integer> {
 						"from Users u where u not in (select distinct uh.users from UserHierarchy uh) order by u.id");
 		return query.list();
 
+	}
+
+	public Integer countByTerm(String term) {
+		Query query = getSession()
+				.createQuery(
+						"select count(*) from Users u where u.name like :term or u.surname like :term or u.email like :term or u.username like :term");
+		query.setParameter("term", term + LIKE_OPERATOR);
+
+		return ((Long) query.uniqueResult()).intValue();
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Users> findByTerm(String term, Integer firstResult,
+			Integer maxResults) {
+
+		Query query = getSession()
+				.createQuery(
+						"from Users u where u.name like :term or u.surname like :term or u.email like :term or u.username like :term");
+		query.setParameter("term", term + LIKE_OPERATOR);
+
+		if (firstResult != null) {
+			query.setFirstResult(firstResult);
+		}
+		if (maxResults != null) {
+			query.setMaxResults(maxResults);
+		}
+
+		return query.list();
 	}
 
 }
