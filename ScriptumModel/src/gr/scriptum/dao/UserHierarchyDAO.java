d92a8834bb459d866318df946994d31f654d3c54
Angelos Anagnostopoulos <angelos@uit.gr>
2011-10-28 18:28:55 +0000

    Added method which, given a manager and a project, returns the manager's subordinates that also participate in that project.

--- ScriptumModel/src/gr/scriptum/dao/UserHierarchyDAO.java
+++ ScriptumModel/src/gr/scriptum/dao/UserHierarchyDAO.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.hibernate.Query;
 
 import gr.scriptum.domain.Department;
+import gr.scriptum.domain.Project;
 import gr.scriptum.domain.UserHierarchy;
 import gr.scriptum.domain.Users;
 
@@ -37,10 +38,19 @@ public class UserHierarchyDAO extends GenericDAO<UserHierarchy, Integer> {
 		return true;
 	}
 
-	public List<UserHierarchy> findByUser(Users user) {
+	public List<UserHierarchy> findByUser(Users manager) {
 		Query query = getSession().createQuery(
 				"from UserHierarchy uh where uh.users = :user order by uh.id");
-		query.setParameter("user", user);
+		query.setParameter("user", manager);
+		return query.list();
+	}
+
+	public List<UserHierarchy> findByUser(Users manager, Project project) {
+		Query query = getSession()
+				.createQuery(
+						"select uh from UserHierarchy uh, ProjectUser pj where uh.users = pj.id.users and uh.users = :user and pj.id.project = :project order by uh.id");
+		query.setParameter("user", manager);
+		query.setParameter("project", project);
 		return query.list();
 	}
 }
