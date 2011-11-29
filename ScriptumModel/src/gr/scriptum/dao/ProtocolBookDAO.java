a78a9ecb09bad1e019e79889c2116c459800a43b
Angelos Anagnostopoulos <angelos@uit.gr>
2011-11-29 10:12:43 +0000

    Added specific save/isDeletable/delete functionality.

--- ScriptumModel/src/gr/scriptum/dao/ProtocolBookDAO.java
+++ ScriptumModel/src/gr/scriptum/dao/ProtocolBookDAO.java
@@ -4,12 +4,15 @@
 package gr.scriptum.dao;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 
 import gr.scriptum.domain.ProtocolBook;
+import gr.scriptum.domain.ProtocolNumber;
+import gr.scriptum.domain.ProtocolNumber.ProtocolNumberType;
 
 /**
  * @author aanagnostopoulos
@@ -25,5 +28,41 @@ public class ProtocolBookDAO extends GenericDAO<ProtocolBook, Integer> {
 		return findByExample(protocolBook, MatchMode.ANYWHERE, sortBy, null,
 				null);
 
+	}
+
+	@Override
+	public boolean isDeletable(Integer id) {
+
+		ProtocolBook book = findById(id, false);
+		if (!book.getIncomingProtocols().isEmpty()
+				|| !book.getOutgoingProtocols().isEmpty()) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public ProtocolBook makePersistent(ProtocolBook entity) {
+		ProtocolBook protocolBook = super.makePersistent(entity);
+		
+		//create relevant Protocol Number
+		ProtocolNumberDAO protocolNumberDAO =  new ProtocolNumberDAO();
+		ProtocolNumber protocolNumber = protocolNumberDAO.createProtocolNumber(protocolBook);
+		protocolBook.getProtocolNumbers().add(protocolNumber);
+		return protocolBook;
+	}
+	
+	@Override
+	public void deleteById(Integer id) {
+
+		ProtocolBook book = findById(id, false);
+
+		// delete relevant ProtocolNumbers
+		ProtocolNumberDAO protocolNumberDAO = new ProtocolNumberDAO();
+		for (ProtocolNumber protocolNumber : book.getProtocolNumbers()) {
+			protocolNumberDAO.delete(protocolNumber);
+		}
+		// delete book itself
+		super.delete(book);
 	}
 }
