b11a87c984fab37a98c5e118eb59f740179fd99c
Mike Mountrakis <mountrakis@uit.gr>
2011-12-01 10:05:44 +0000

    created

---
+++ eCase/src/gr/scriptum/ecase/controller/reports/TasksWaitingExternalController.java
@@ -0,0 +1,20 @@
+package gr.scriptum.ecase.controller.reports;
+
+
+import gr.scriptum.dao.reports.ReportTaskPerProjectDAO;
+import gr.scriptum.domain.reports.TaskPerProject;
+
+
+/**
+ * @author Michael Mountrakis mountrakis@uit.gr <br>
+ * Date: Dec 1, 2011 11:09:00 AM <br>
+ * Project: SCRIPTUM http://www.scriptum.gr <br>
+ * http://www.uit.gr <br>
+   MySQL Report for all tasks (and free tasks) that have been assigned by this user from all his projects
+   to all sub users that are in state 3 = waiting from an external contact.
+ */
+public class TasksWaitingExternalController  extends GenericReportController<TaskPerProject, ReportTaskPerProjectDAO>{
+
+	private static final long serialVersionUID = -7816129622700754097L;
+
+}
