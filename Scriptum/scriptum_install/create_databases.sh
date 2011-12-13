cee1d032da00065ef53011e405834664ff937753
Mike Mountrakis <mountrakis@uit.gr>
2011-12-13 07:25:27 +0000

    Updated to user SCRIPTUM instead of ELLAK

--- Scriptum/scriptum_install/create_databases.sh
+++ Scriptum/scriptum_install/create_databases.sh
@@ -3,8 +3,9 @@ rootpass=xxx
 #-------------------------------------------
 echo Creating the SCRIPTUM databases
 echo Connect as root using password $rootpass
-mysql -u root --password=$rootpass < ellak.sql
+mysql -u root --password=$rootpass < scriptum.sql
 mysql -u root --password=$rootpass < okm_app.sql
 mysql -u root --password=$rootpass < okm_repo.sql
 mysql -u root --password=$rootpass < scriptum_users.sql
+
 
